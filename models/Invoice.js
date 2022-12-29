45857acaee8579367d0c5a5f6186f739584521c7
Dana Arroway <dana.arroway@gmail.com>
2022-12-29 13:14:30 -0600

    added due and overdue props to Invoice model so can track invoice amounts - should also help with finance charges and interest

--- models/Invoice.js
+++ models/Invoice.js
@@ -5,6 +5,7 @@ const InvoiceSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    unique: true,
   },
   date: {
     type: Date,
@@ -21,15 +22,24 @@ const InvoiceSchema = new mongoose.Schema({
   },
   image: {
     type: String,
-    require: true,
+    required: true,
   },
   cloudinaryId: {
     type: String,
-    require: true,
+    required: true,
   },
   isPaid: {
     type: Boolean,
     default: false,
+  },
+  due: {
+    type: Number,
+    required: true,
+  },
+  overDue: {
+    type: Number,
+    required: true,
+    default: 0.00,
   },
   paidBy: {
     type: mongoose.Schema.Types.ObjectId,
